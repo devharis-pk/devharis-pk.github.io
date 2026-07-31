41f237a2f725ce89f2f24e5b06bffed34b0bd2c2
Muhammad Haris <haris.dev.pk@gmail.com>
2026-08-01 02:52:52 +0500

    Add responsive navigation script

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.querySelector(".menu-btn")?.addEventListener("click",()=>document.querySelector(".nav")?.classList.toggle("open"));document.querySelectorAll(".nav a").forEach(a=>a.addEventListener("click",()=>document.querySelector(".nav")?.classList.remove("open")));document.getElementById("year").textContent=new Date().getFullYear();
